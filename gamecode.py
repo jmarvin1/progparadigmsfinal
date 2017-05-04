25c3a8203453eba6a491cdc07a6158e558df7c55
James Marvin <jmarvin1@nd.edu>
2017-05-04 17:18:50 -0400

    updated gamecode with twisted stuff

--- gamecode.py
+++ gamecode.py
@@ -1,3 +1,6 @@
+from twisted.internet.protocol import ClientFactory
+from twisted.internet.protocol import Protocol
+from twisted.internet import reactor
 import sys, pygame
 import math
 
@@ -137,6 +140,36 @@ class colorIndex(pygame.sprite.Sprite):
 	def __init__(self):
 		pygame.sprite.Sprite.__init__(self)
 		
+class ServerConnection(Protocol):
+	def __init__(self, addr, connection):
+		self.addr=addr
+		self.connection=connection
+
+	def connectionMade(self):
+		print( 'connection made')
+
+	def dataReceived(self, data):
+		print( 'data: ', data)
+
+	def connectionLost(self, reasonForDisconnect):
+		print('connection lost-reason: ', reasonForDisconnect)
+		try:
+			reactor.stop()
+		except:
+			pass
+
+class ServerConnectionFactory(ClientFactory):
+	def __init__(self, connection):
+		self.connection=connection
+
+	def buildProtocol(self, addr):
+		return ServerConnection(addr, self.connection)
+
+class Connections(object):
+	def run(self):
+		reactor.connectTCP("ash.campus.nd.edu", 40043, ServerConnectionFactory(self))
+		reactor.run()
+
 	
 
 if __name__ == "__main__":
@@ -144,6 +177,7 @@ if __name__ == "__main__":
 	#s=ship(gs)
 	gs.run()
 	#s = ship(gs)
-	
+	connection=Connections()
+	connection.run()
 	
 		
