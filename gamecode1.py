851c641e6b56222a8198e1b6bef30f296e3d9047
James Marvin <jmarvin1@nd.edu>
2017-05-05 15:46:21 -0400

    gamecode1

---
+++ gamecode1.py
@@ -0,0 +1,84 @@
+from twisted.internet.protocol import ClientFactory
+from twisted.internet.protocol import Protocol
+from twisted.internet import reactor
+from twisted.internet.task import LoopingCall
+from twisted.protocols.basic import LineReceiver
+import sys, pygame
+
+class ServerConnection(Protocol):
+    def __init__(self):
+        pygame.init()
+        pygaem.keys.set_repeat(1,30) #helps for drag feature
+        self.size=800,800
+        self.screen= pygame.display.set_mode(self.size)
+        self.color= 0,0,0
+        self.gs=None
+        self.screen.fill(self.color)
+        self.color = 255,0,0
+        self.colorstring="red"
+        reactor.callLater(.05, self.tick)
+    def tick(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                reactor.stop()
+            if event.type == pygame.KEY_DOWN:
+                if event.key == pygame.K_g:
+                    self.color = 0,255,0
+                    self.colorstring="green"
+                if event.key == pygame.K_b:
+                    self.color = 0,0,255
+                    self.colorstring="blue"
+                if event.key == pygame.K_r:
+                    self.color = 255, 0, 0
+                    self.colorstring = "red"
+                if event.key == pygame.K_p:
+                    self.color = 254, 143, 194
+                    self.colorstring="pink"
+                if event.key == pygame.K_y:
+                    self.color = 255, 255, 0
+                    self.colorstring="yellow"
+            if event.type == pygame.MOUSE_BUTTONDOWN:
+                self.transport.write("draw circle: "+self.color+":"+pygame.mouse.get_pos()+"\r\n")
+
+        pygame.display.flip()
+        reactor.callLater(.05, self.tick)
+
+    def connectionMade(self):
+        print( 'connection made')
+
+    def dataReceived(self, data):
+        print( 'data: ', data)
+
+    def connectionLost(self, reasonForDisconnect):
+        print('connection lost-reason: ', reasonForDisconnect)
+        try:
+            reactor.stop()
+        except:
+            pass
+
+    def lineReceived(self, line):
+        print('line received')
+        line.split(':')
+        if line[0]=='draw circle':
+            pygame.draw_circle(self.screen,line[1],line[2],5,2)
+
+class ServerConnectionFactory(ClientFactory):
+    def __init__(self):
+        self.myconn=ServerConnection()
+        #self.connection=connection
+
+    def buildProtocol(self, addr):
+        #return ServerConnection(addr, self.connection)
+        return self.myconn
+
+class Connections(object):
+    def run(self):
+        reactor.connectTCP("ash.campus.nd.edu", 40043, ServerConnectionFactory())
+        reactor.run()
+
+if __name__ == "__main__":
+    reactor.connectTCP("ash.campus.nd.edu", 40043, connection)
+    reactor.run()
+
+
+
